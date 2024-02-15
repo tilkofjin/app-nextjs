f482750336b6de580e2b58c5477ee24ad797b1f5
tilkofjin <tilkofjin@163.com>
2024-02-15 23:24:17 +0800

    change footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,8 +1,16 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='border-t'>
+      <div>
+        <Link href='/'>
+          <Image src='/assets/images/logo.svg' alt='logo' width={128} height={24} />
+        </Link>
+      </div>
+    </footer >
   )
 }
 
