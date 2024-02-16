24728d78e30a42b0969df913e6b1a0cefd5cc265
tilkofjin <tilkofjin@163.com>
2024-02-16 22:49:29 +0800

    changed root page

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -5,10 +5,11 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='border-t'>
-      <div>
+      <div className='wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row'>
         <Link href='/'>
           <Image src='/assets/images/logo.svg' alt='logo' width={128} height={24} />
         </Link>
+        <p>2024 Evently. All Rights reserved.</p>
       </div>
     </footer >
   )
